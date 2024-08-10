6b31b02b69cd92eeff39c414b8b6254a5b93b355
GusDuqRam <122504109+GusDuqRam@users.noreply.github.com>
2024-08-10 17:35:05 -0500

    feat: template literal

--- es6.js
+++ es6.js
@@ -1,17 +1,27 @@
 //template literals
 
-function saludar(nombre){
-    return "hola "+ nombre;
+function inicio (){
+    function saludar(nombre){
+        return "hola "+ nombre;
+    }
+
+    function calcularNacimiento(edad){
+        return 2023-edad;
+    }
+
+    let nombrePersona = prompt("ingresa tu nombre");
+    let resultadoSaludo = saludar(nombrePersona);
+
+    let edadPersona = prompt("ingresa tu edad");
+    let resultadoCalculo = calcularNacimiento(edadPersona);
+
+//let result = "Hola " + resultadoSaludo + "tu año de nacimiento es: " +resultadoCalculo;
+
+// esto es un template literal
+    let result2 = `${resultadoSaludo} tu año de nacimiento es ${resultadoCalculo}`;
+
+    console.log(result2);
 }
 
-function calcularNacimiento(edad){
-    return 2023-edad;
-}
-
-let nombrePersona = prompt("ingresa tu nombre");
-let resultadoSaludo = saludar(nombrePersona);
-
-let edadPersona = prompt("ingresa tu edad");
-let resultadoCalculo = calcularNacimiento(edadPersona);
-console.log("Hola " + resultadoSaludo + "tu año de nacimiento es: " +resultadoCalculo);
+inicio();
 
