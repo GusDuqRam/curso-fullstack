b0b054adea977b0bf854340625aaf263c9c7aa25
GusDuqRam <122504109+GusDuqRam@users.noreply.github.com>
2024-08-10 18:09:29 -0500

    feat: array methods

--- es6.js
+++ es6.js
@@ -23,5 +23,45 @@ const inicio = () =>{
     console.log(result2);
 }
 
-inicio();
+//inicio();
+
+// IIFE
+//inmediately invoked  fuction expression
+// para funciones que se cargen una vez en la pagina
+(function () {
+    console.log("hola mundo");
+})();
+
+// arrays
+const baloto = [18, 28, 38, 80, 90]
+console.log(baloto);
+
+//agregar un elemento al final del array
+baloto.push(30);
+console.log(baloto);
+
+//agregar un elemento al inicio del array
+baloto.unshift(4);
+console.log(baloto);
+
+//eliminar el ultimo elemento del array
+baloto.pop();
+console.log(baloto);
+
+//eliminar el primer elemento del array
+baloto.shift();
+console.log(baloto);
+
+//map podemos retornar el valor de una variable
+const array2 = baloto.map((el) => el = el*2);
+console.log(array2);
+
+//forEach no podemos retornar el valor de una variable
+baloto.forEach((el) => {
+    console.log(el);
+});
+
+//filter podemos filtrar los elementos de una array
+const filtersNumbers = baloto.filter((el) => el >= 30);
+console.log(filtersNumbers);
 
