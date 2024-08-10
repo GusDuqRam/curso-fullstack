14d42a4aa079ccb4204e04ccb19e3f76de3a31c7
GusDuqRam <122504109+GusDuqRam@users.noreply.github.com>
2024-08-10 17:25:18 -0500

    feat: s

---
+++ es6.js
@@ -0,0 +1,16 @@
+//template literals
+
+function saludar(nombre){
+    return "hola "+ nombre;
+}
+
+function calcularNacimiento(edad){
+    return 2023-edad;
+}
+
+let nombrePersona = prompt("ingresa tu nombre");
+let resultadoSaludo = saludar(nombrePersona);
+
+let edadPersona = prompt("ingresa tu edad");
+let resultadoCalculo = calcularNacimiento(edadPersona);
+console.log("Hola " + resultadoSaludo + "tu año de nacimiento es: " +resultadoCalculo);
