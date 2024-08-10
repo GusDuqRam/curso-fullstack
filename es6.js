26fcdd2348fdfa26bf866741a59109e43a9180e1
GusDuqRam <122504109+GusDuqRam@users.noreply.github.com>
2024-08-10 17:44:36 -0500

    feat: arrow fuctions

--- es6.js
+++ es6.js
@@ -1,16 +1,16 @@
 //template literals
 
-function inicio (){
-    function saludar(nombre){
+const inicio = () =>{
+    //funcion tradicional
+    function saludar2(nombre){
         return "hola "+ nombre;
     }
 
-    function calcularNacimiento(edad){
-        return 2023-edad;
-    }
+    //funcion flecha o arrow function
+    const saludar = (nombre) => `hola ${nombre}`;
+    const calcularNacimiento = (edad) => 2023-edad;
 
     let nombrePersona = prompt("ingresa tu nombre");
-    let resultadoSaludo = saludar(nombrePersona);
 
     let edadPersona = prompt("ingresa tu edad");
     let resultadoCalculo = calcularNacimiento(edadPersona);
@@ -18,7 +18,7 @@ function inicio (){
 //let result = "Hola " + resultadoSaludo + "tu año de nacimiento es: " +resultadoCalculo;
 
 // esto es un template literal
-    let result2 = `${resultadoSaludo} tu año de nacimiento es ${resultadoCalculo}`;
+    let result2 = `${saludar(nombrePersona)} tu año de nacimiento es ${resultadoCalculo}`;
 
     console.log(result2);
 }
